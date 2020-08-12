02c7b575582c0c058e29083f8c41655395f3f655
Ayman Alrifai <ayman97rf@gmail.com>
2020-08-12 07:04:47 +0200

    Edits in Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,47 @@
 var toggle = document.querySelector('.toggle');
-var menu = document.querySelector('.nav__list');
+var navOverlayed = document.querySelector('.no-overlay .nav__list');
+var mainNav = document.querySelector('#main-header .nav');
+var mainNavList = document.querySelector('#main-header .nav .nav__list');
 var overlay = document.querySelector('.no-overlay');
 
+// Changing Navbar on Scroll
+window.onscroll = function () {
+    console.log(document.documentElement.scrollTop);
+    if ((window.pageYOffset || document.documentElement.scrollTop >= 200) && !overlay.classList.contains("overlay")) {
+      mainNav.classList.add("nav-colored");
+      mainNav.classList.remove("nav-transparent");
+      toggle.classList.add("toggle--on-bg");
+    } else {
+
+      mainNav.classList.add("nav-transparent");
+      mainNav.classList.remove("nav-colored");
+      toggle.classList.remove("toggle--on-bg");
+    }
+};
+
+// Hamburger Menu
 function toggleMenu() {
-    if (menu.classList.contains("active")) {
+    if (navOverlayed.classList.contains("active")) {
         console.log('Here')
         if (window.innerWidth > 960) {
           toggle.style.display = "none";
         }
 
-        menu.classList.remove("active");
+        navOverlayed.classList.remove("active");
         overlay.classList.remove("overlay");
         overlay.classList.add("no-overlay");
-
+        mainNavList.classList.remove("overlay-mode");
 
         toggle.querySelector("a").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.1" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/></svg>';
+      
       } else {
         
 
-        menu.classList.add("active");
+        navOverlayed.classList.add("active");
         overlay.classList.remove("no-overlay");
         overlay.classList.add("overlay");
+        mainNavList.classList.add("overlay-mode");
+        console.log('Hey')
 
         toggle.querySelector("a").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" version="1.1" viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/></svg>';
       }
@@ -31,7 +52,6 @@ window.addEventListener('resize', function(event){
   console.log(window.innerWidth)
   if (window.innerWidth <= 960 || (overlay.classList.contains("overlay") && window.innerWidth > 960)) {
     toggle.style.display = "block";
-    console.log('HAAA')
   } else {
     toggle.style.display = "none";
   }
